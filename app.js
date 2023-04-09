7a35b6d47591620bd3b5b2c097ce701b08e10cc6
Wesley Willian <wesleywilliancg@gmail.com>
2023-04-09 15:52:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+new Vue({
+    el: "#desafio",
+    data: {
+        jogoIniciado: false,
+        desistir: false,
+        vidaJogador: 100,
+        vidaMonstro: 100,
+        battleLog: [],
+    },
+    
+    methods: {
+        resetarJogo() {
+            this.jogoIniciado = false;
+        },
+        atacar() {
+            const danoJogador = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+            this.vidaMonstro -= danoJogador;
+            if (this.vidaMonstro < 0) {
+                this.vidaMonstro = 0;
+                this.resetarJogo();
+            } 
+            this.monstroAtacar();
+            const log = {
+                type: 'player',
+                message: `JOGADOR ATINGIU MONSTRO COM ${danoJogador}.`
+            };
+            this.battleLog.unshift(log);
+            console.log('jogoIniciado', this.jogoIniciado);
+        },
+        ataqueEspecial() {
+            const danoJogador = Math.floor(Math.random() * (10 - 5 + 1)) + 10;
+            this.vidaMonstro -= danoJogador;
+            if (this.vidaMonstro < 0) {
+                this.vidaMonstro = 0;
+                this.resetarJogo();
+            }
+            this.monstroAtacar();
+            const log = {
+                type: 'player',
+                message: `JOGADOR ATINGIU MONSTRO COM ${danoJogador}.`
+            };
+            this.battleLog.unshift(log);
+        },
+        monstroAtacar() {
+            const danoMonstro = Math.floor(Math.random() * (12 - 7 + 1)) + 7;
+            this.vidaJogador -= danoMonstro;
+            if (this.vidaJogador < 0) {
+                this.vidaJogador = 0;
+                this.resetarJogo();
+            }
+            const log = {
+                type: 'monster',
+                message: `MONSTRO ATINGIU JOGADOR COM ${danoMonstro}.`
+            };
+            this.battleLog.unshift(log);
+        },
+        iniciar() {
+            if (this.vidaJogador <= 0 || this.vidaMonstro <= 0) {
+                this.jogoIniciado = false;
+            } else {
+                this.jogoIniciado = true;
+            }
+            this.vidaJogador = 100;
+            this.vidaMonstro = 100;
+            this.battleLog = [];
+        },
+        curar() {
+            if (this.vidaJogador < 90) {
+                const totalRecuperado = Math.floor(Math.random() * (12 - 7 + 1)) + 9;
+                this.vidaJogador += totalRecuperado;
+                const message = `JOGADOR GANHOU FORÇA DE ${totalRecuperado}.`;
+                this.battleLog.unshift({ type: "player", message });
+            } else {
+                this.battleLog.unshift({ type: "player", message: "Jogador já está com a vida cheia." });
+            }
+            this.monstroAtacar();
+        },
+
+    }
+});
